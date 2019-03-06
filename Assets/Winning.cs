1d312190fbe6f7755dd66a325e74d94809b516a4
Audrey-L. St-Louis <audreylaurestlouis@gmail.com>
2019-03-06 08:06:31 -0500

    Winning feature

---
+++ Assets/Winning.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winning : MonoBehaviour
+{
+    public GameObject won;
+    // Start is called before the first frame update
+    void Start()
+    {
+        won.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        won.SetActive(true);
+    }
+}
