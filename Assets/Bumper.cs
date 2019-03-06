e5bca9da06d523bfba6f23410a60c0b8a15b49a1
Audrey-L. St-Louis <audreylaurestlouis@gmail.com>
2019-03-05 19:59:24 -0500

    Changes made to the bumpers and the door

--- Assets/Bumper.cs
+++ Assets/Bumper.cs
@@ -6,13 +6,15 @@ public class Bumper : MonoBehaviour
 {
     public Score thescore;
     public DoorOpening theDoor;
+   
+    public static int num = 0;
 
     private int timer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log("Value of the variable num at the start=" + num + " ");
     }
 
     // Update is called once per frame
@@ -27,25 +29,44 @@ public class Bumper : MonoBehaviour
             Debug.Log(timer);
             timer--;
         }
+
+        Debug.Log("Value of the variable during game=" + num + " ");
+
+        //if (this.GetComponent<Renderer>().material.color == Color.red)
+        if (num == 3)
+        {
+            theDoor.GetComponent<DoorOpening>().enabled = true;
+           
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("enter");
+        //Debug.Log("enter");
         if (timer == 0)
         {
             thescore.AddScore(5);
             thescore.Addmultiplier(1);
         }
-        // if bumper is red when enter a collision, 
-        if (this.GetComponent<Renderer>().material.color == Color.red)
-        {
-            theDoor.GetComponent<DoorOpening>().enabled=true;
-
-        }
+        // if bumper is red when enter a collision,
+       
 
         this.GetComponent<AudioSource>().Play();
         this.GetComponent<ConstantForce>().enabled = true;
         timer = 3;
+    
+    
+    }
+
+    public static void increaseNum()
+    { 
+        num++;
+        Debug.Log("VALUE OF NUM WHEN INCREASE NUM IS CALLED = "+ num);
+    }
+    public static void decreaseNum()
+    {   
+         num--;
+        Debug.Log("VALUE OF NUM WHEN DECREASE NUM IS CALLED = " + num);
     }
 }
--- Assets/ColorChange.cs
+++ Assets/ColorChange.cs
@@ -2,35 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ColorChange : MonoBehaviour
 {
-   
+
     float timer;
-    public GameObject rb; 
-    Color[] color = new Color[4];
+  //  public GameObject rb;
+    Color[] color = new Color[2];
     int index = 0;
+
+
     void Start()
-    {   //rb= GameObject.Find("Bumper");
-        color[0] = Color.blue;
-        color[1] = Color.red;
-        color[2] = Color.yellow;
-        color[3] = Color.green;
+    {   
+        color[0] = Color.red;
+        color[1] = Color.green;
+
         this.GetComponent<Renderer>().material.color = color[index];
     }
 
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer > 2)
-        {
-            if (index < 1)
-                index++;
-            else
-                index = 0; 
-            this.GetComponent<Renderer>().material.color = color[index];
-            timer = 0;
-        }
+        //timer += Time.deltaTime;
+        //if (timer > 2)
+        //{
+        //    if (index < 1)
+        //        index++;
+        //    else
+        //        index = 0;
+        //    this.GetComponent<Renderer>().material.color = color[index];
+        //    timer = 0;
+        //}
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (index < 1)
+            index++;
+        else
+            index = 0;
+        this.GetComponent<Renderer>().material.color = color[index];
 
+          if(index==1)              //if(this.GetComponent<Renderer>().material.color == color[1])
+            Bumper.increaseNum();
+        else
+            Bumper.decreaseNum();
 
+    }
 }
