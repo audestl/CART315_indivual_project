52ff6ac3d0981e9fd67cc3d35a3b33623863207d
Audrey-L. St-Louis <audreylaurestlouis@gmail.com>
2019-03-27 08:45:16 -0400

    Added more bumpers

---
+++ Assets/BoringBumper.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public DoorOpening theDoor;
+
+
+private int timer = 0;
+
+
+
+
+void Start()
+{
+
+
+}
+
+// Update is called once per frame
+void FixedUpdate()
+{
+    if (timer == 0)
+    {
+        this.GetComponent<ConstantForce>().enabled = false;
+
+     
+
+    }
+    else
+    {
+    
+
+        timer--;
+    }
+
+
+}
+
+private void OnCollisionEnter(Collision collision)
+{
+
+    if (timer == 0)
+    {
+
+     
+        thescore.AddScore(5);
+
+
+    }
+
+
+
+    this.GetComponent<AudioSource>().Play();
+    this.GetComponent<ConstantForce>().enabled = true;
+    timer = 3;
+
+
+}
+
+
+
+  
+
+
+}
